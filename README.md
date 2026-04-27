88c4ea0dc770e2a29443fc508272fd29c91ab02d
Daniel Lopez <dlope073@gmail.com>
2026-04-26 22:28:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My hello world switch application
+
+Just prints some random text and a random seed value using some inline arm assembly
---
+++ source/main.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <switch.h>
+
+uint64_t hardware_random_seed(void) {
+    uint64_t seed = 0;
+    
+    __asm ("MRS %[seed], RNDRRS" : [seed] "=r" (seed));
+   
+    return seed;
+}
+
+int main(int argc, char **argv)
+{
+    //Initialize console. Using NULL as the second argument tells the console library to use the internal console structure as current one.
+    consoleInit(NULL);
+
+    // Configure our supported input layout: a single player with standard controller styles
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
+    PadState pad;
+    padInitializeDefault(&pad);
+
+    //Move the cursor to row 16 and column 20 and then prints "Hello World!"
+    //To move the cursor you have to print "\x1b[r;cH", where r and c are respectively
+    //the row and column where you want your cursor to move
+    printf("\x1b[0;0HPinga madre I compiled this switch app \"sin AI\"!");
+    printf("\x1b[1;0HRandom seed=%zu", hardware_random_seed());
+
+    while(appletMainLoop())
+    {
+        // Scan the gamepad. This should be done once for each frame
+        padUpdate(&pad);
+
+        // padGetButtonsDown returns the set of buttons that have been newly pressed in this frame compared to the previous one
+        u64 kDown = padGetButtonsDown(&pad);
+
+        if (kDown & HidNpadButton_Plus) break; // break in order to return to hbmenu
+
+        consoleUpdate(NULL);
+    }
+
+    consoleExit(NULL);
+    return 0;
+}

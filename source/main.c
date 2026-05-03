8f4b19e227068e6684f0d8620284eb433421b630
Daniel Lopez <dlope073@gmail.com>
2026-05-03 19:39:33 -0400

    Add a working arm instruction

--- source/main.c
+++ source/main.c
@@ -1,48 +1,42 @@
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
 #include <switch.h>
 
+//Showing some of my arm-fu -- unfortunately this homebrew app runs at exception level 0 so I cannot do too much magic :(
+
+static inline volatile uint64_t get_arm64_system_tick(void) {
+    uint64_t ticks;
+    __asm__ __volatile__ ("MRS %x[data], CNTPCT_EL0" : [data] "=r" (ticks));
+    return ticks;
+}
+
+//Source: https://developer.arm.com/documentation/ddi0601/2026-03/AArch64-Registers/RNDRRS--Random-Number-Full-Entropy
+//Undefined on the switch for some reason -- not sure cannot check since this app is not on EL0
+static inline volatile uint64_t get_rndrrs_special_arm_register_value(void) {
+    uint64_t seed;
+    __asm__ __volatile__ ("MRS %x[data], RNDRRS" : [data] "=r" (seed));
+    return seed;
+}
 
 int main(int argc, char **argv)
 {
-    //Initialize console. Using NULL as the second argument tells the console library to use the internal console structure as current one.
     consoleInit(NULL);
-
-    // Configure our supported input layout: a single player with standard controller styles
     padConfigureInput(1, HidNpadStyleSet_NpadStandard);
 
-    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
     PadState pad;
     padInitializeDefault(&pad);
 
-    //Move the cursor to row 16 and column 20 and then prints "Hello World!"
-    //To move the cursor you have to print "\x1b[r;cH", where r and c are respectively
-    //the row and column where you want your cursor to move
-    printf("\x1b[0;0HPinga madre I compiled this switch app \"sin AI\"!");
-    
-    // A little demonstration of my arm powers jk
-    uint64_t hardware_random_seed = 0;
-    __asm__ volatile(
-        "mrs %x0, RNDRRS\n"      
-        "str %x0, [%0]\n"    
-        : "=r"(hardware_random_seed)                 
-    );
-    
-    
-    // Crashes when I access probably doing something wrong or the switch not supporting it 
-    //printf("\x1b[10;0HRandom seed=%zu", hardware_random_seed);
 
-    while(appletMainLoop())
-    {
-        // Scan the gamepad. This should be done once for each frame
+    printf("\x1b[0;0H  CNTPCT_EL0=%zu", get_arm64_system_tick());
+
+    while(appletMainLoop()) {
         padUpdate(&pad);
-
-        // padGetButtonsDown returns the set of buttons that have been newly pressed in this frame compared to the previous one
         uint64_t kDown = padGetButtonsDown(&pad);
-
-        if (kDown & HidNpadButton_Plus) break; // break in order to return to hbmenu
-
+        if (kDown & HidNpadButton_Plus) {
+            break;
+        }
         consoleUpdate(NULL);
     }
 
